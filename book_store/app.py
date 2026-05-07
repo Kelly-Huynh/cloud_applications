fec3a4906f14175e2f98bf783480f4cd290f95cb
Kelly-Huynh <96kellyhuynh@gmail.com>
2026-05-07 11:26:39 +0100

    Initial commit of Iteration 1

---
+++ book_store/app.py
@@ -0,0 +1,76 @@
+from flask import Flask
+from flask import render_template
+
+# instantiate a Flask app object
+app = Flask(__name__)
+
+# Declares a route that listens for a GET request to the path /hello
+# and a method to execute when that request comes in
+@app.route('/hello', methods=['GET'])
+def hello():
+    return "Hello to you too"
+
+# # NEW PART START
+
+# # duplicate route
+# @app.route('/hello', methods=['GET'])
+# def hello_again():
+#     return "Hello, hello and hello again!"
+
+# # NEW PART END
+
+@app.route('/books', methods=['GET'])
+def books():
+    return render_template("books.html")
+    # book_list = [
+    #     {
+    #         "title": "The Gruffalo",
+    #         "author": "Julia Donaldson"
+    #     },
+    #     {
+    #         "title": "Ada Twist, Scientist",
+    #         "author": "Andrea Beaty"
+    #     },
+    #     {
+    #         "title": "The Girl Who Drank the Moon",
+    #         "author": "Kelly Barnhill"
+    #     },
+    #     {
+    #         "title": "Dragons in a Bag",
+    #         "author": "Zetta Elliott"
+    #     }
+    # ]
+    # return book_list
+
+@app.route('/authors', methods=['GET'])
+def authors():
+    author_list = [
+        {
+        "name": "Julia Donaldson",
+        "dob": "1948-09-16"
+        },
+        {
+            "name": "Andrea Beaty",
+            "dob": "1961-10-08"
+        },
+        {
+            "name": "Kelly Barnhill",
+            "dob": "1973-01-01"
+        },
+        {
+            "name": "Zetta Elliott",
+            "dob": "1979-11-11"
+        }
+    ]
+    return author_list
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+# make the server run in response to `python app.py`
+# on port 5001 (you'll learn more about what this means later)
+# and use debug mode so that changing code restarts the app
+if __name__ == "__main__":
+    app.run(port=5001, debug=True)
+
